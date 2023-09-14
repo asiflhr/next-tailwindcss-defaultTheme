f78f235bbdb6e1454361b42cc0b3613f581eff1a
Muhammad Asif <asif88383@gmail.com>
2023-09-14 23:36:32 +0500

    'added-button-component'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 // src/index.js
 const plugin = require('tailwindcss/plugin')
-
+const Button = require('./components/buttons')
 const components = require('../dist/components')
 const utilities = require('../dist/utilities')
 
-const TailwindUiKitPlugin = plugin(
-  function ({ addUtilities, addComponents }) {
-    addUtilities(utilities)
-    addComponents(components)
-  },
-)
+const TailwindUiKitPlugin = plugin(function ({ addUtilities, addComponents }) {
+  addUtilities(utilities)
+  addComponents(components)
+})
 
-module.exports = TailwindUiKitPlugin
+module.exports = [TailwindUiKitPlugin, Button]
